6b3c7db69b2239324024b0db4a45b8027b8760bc
houlei <houlei@mail.jj.cn>
2016-02-17 10:07:52 +0800

    new year

--- hello.py
+++ hello.py
@@ -369,11 +369,182 @@ print(s)
 print(abs(100))#绝对值函数
 print(abs(-100))
 print(abs(10.02))
+max(1,2,3)
+print(max(1,2,3))#max函数max()可以接收任意多个参数，并返回最大的那个
+print(max(2, 3, 1, -5))
+print('数据类型转换')
+print(int('123'))
+print(int(12.34))
+print( float('12.34'))
+print(str(1.23))
+print(bool(1))
+print(bool(''))
+print('函数名其实就是指向一个函数对象的引用，完全可以把函数名赋给一个变量，相当于给这个函数起了一个“别名”')
+a = abs # 变量a指向abs函数
+print(a(-1)) # 所以也可以通过a调用abs函数
+#n1 = int(input('请输入一个整数：'))
+#print(hex(n1))
+#n1 = input('请输入一个整数：')
+#print(hex(n1))
+#n2 = int(input('请输入另一个整数：'))
+#print(hex(n2))
+print('\n定义函数')
+print('在Python中，定义一个函数要使用def语句，依次写出函数名、括号、括号中的参数和冒号:，然后，在缩进块中编写函数体，函数的返回值用return语句返回')
+#def my_abs(x):
+#    if x >= 0:
+#        return x
+#    else:
+#        return -x
+#print(my_abs(-102))
+from abstest import my_abs
+print(my_abs(-102))
+print(my_abs(-1))
+print('''空函数
+    如果想定义一个什么事也不做的空函数，可以用pass语句''')
+def nop():
+    pass
+print('pass语句什么都不做，那有什么用？实际上pass可以用来作为占位符，比如现在还没想好怎么写函数的代码，就可以先放一个pass，让代码能运行起来')
+if age >= 18:
+    pass
+print('''参数检查
+数据类型检查可以用内置函数isinstance()实现''')
+def my_abs(x):
+    if not isinstance(x, (int, float)):
+        raise TypeError('bad operand type')
+    if x >= 0:
+        return x
+    else:
+        return -x
+print(my_abs(-102))
+#print(my_abs('A'))
+import math
 
+def move(x, y, step, angle=0):
+    nx = x + step * math.cos(angle)
+    ny = y - step * math.sin(angle)
+    return nx, ny
+x, y = move(100, 100, 60, math.pi / 6)
+print(x, y)
+r = move(100, 100, 60, math.pi / 6)
+print(r)
+print('原来返回值是一个tuple！但是，在语法上，返回一个tuple可以省略括号，而多个变量可以同时接收一个tuple，按位置赋给对应的值，所以，Python的函数返回多值其实就是返回一个tuple，但写起来更方便')
+print(math.sqrt(2))
+def quadratic(a, b, c):
+    if not isinstance (a,(int,float)):
+        raise TypeError('不能含有字母,请重新输入3个实数')
+    if not isinstance (b,(int,float)):
+        raise TypeError('不能含有字母,请重新输入3个实数')
+    if not isinstance (c,(int,float)):
+        raise TypeError('不能含有字母,请重新输入3个实数')
 
+    if a==0:
+        x=-c/b
+        return x
+    elif (b*b)<(4*a*c):
+        raise TypeError('方程无解 ')
+    else:
+        x1=((-b + math.sqrt(b*b-4*a*c))/(2*a))
+        x2=((-b - math.sqrt(b*b-4*a*c))/(2*a))
+        if x1==x2:
+            return x1
+        else:
+            return x1,x2
 
+#print(quadratic(1,2,3))
+#print(quadratic(a,2,3))
+print(quadratic(1,5,3))
+print(quadratic(0,2,3))
+def power(x, n):
+    s = 1
+    while n > 0:
+        n = n - 1
+        s = s * x
+    return s
+print(power(5, 3))
+def power(x, n=2):
+    s = 1
+    while n > 0:
+        n = n - 1
+        s = s * x
+    return s
+print(power(5))
+print('''默认参数可以简化函数的调用。设置默认参数时，有几点要注意：
 
+一是必选参数在前，默认参数在后，否则Python的解释器会报错（思考一下为什么默认参数不能放在必选参数前面）；
 
+二是如何设置默认参数。
+
+当函数有多个参数时，把变化大的参数放前面，变化小的参数放后面。变化小的参数就可以作为默认参数。''')
+print(power(5,5))
+def enroll(name, gender):
+    print('name:', name)
+    print('gender:', gender)
+print(enroll('Sarah', 'F'))
+
+def enroll(name, gender, age=6, city='Beijing'):
+    print('name:', name)
+    print('gender:', gender)
+    print('age:', age)
+    print('city:', city)
+
+print(enroll('Sarah', 'F'))
+print(enroll('Bob', 'M', 7))
+print(enroll('Adam', 'M', city='Tianjin'))
+
+def add_end(L=[]):
+    L.append('END')
+    return L
+print(add_end([1, 2, 3]))
+print(add_end(['x', 'y', 'z']))
+print(add_end())
+print(add_end())
+print('定义默认参数要牢记一点：默认参数必须指向不变对象！')
+
+def add_end(L=None):
+    if L is None:
+        L = []
+    L.append('END')
+    return L
+
+print(add_end())
+print(add_end())
+print('\n可变参数')
+def calc(numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+
+print(calc([1, 2, 3]))
+print(calc((1, 3, 5, 7)))
+
+def calc(*numbers):#简化
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+print(calc(1, 2, 3))
+print(calc(1,3,5,7))
+print('定义可变参数和定义一个list或tuple参数相比，仅仅在参数前面加了一个*号。在函数内部，参数numbers接收到的是一个tuple，因此，函数代码完全不变。但是，调用该函数时，可以传入任意个参数，包括0个参数')
+print(calc(1, 2))
+print(calc())
+print('python允许你在list或tuple前面加一个*号，把list或tuple的元素变成可变参数传进去')
+nums = [1, 2, 3]
+print(calc(*nums))
+
+print('关键字参数')
+print('可变参数允许你传入0个或任意个参数，这些可变参数在函数调用时自动组装为一个tuple。而关键字参数允许你传入0个或任意个含参数名的参数，这些关键字参数在函数内部自动组装为一个dict')
+def person(name, age, **kw):
+    print('name:', name, 'age:', age, 'other:', kw)
+print(person('Michael', 30))
+print(person('Bob', 35, city='Beijing'))
+print(person('Adam', 45, gender='M', job='Engineer'))
+extra = {'city': 'Beijing', 'job': 'Engineer'}
+print(person('Jack', 24, city=extra['city'], job=extra['job']))
+extra = {'city': 'Beijing', 'job': 'Engineer'}
+print(person('Jack', 24, **extra))#简化写法
+print('**extra表示把extra这个dict的所有key-value用关键字参数传入到函数的**kw参数，kw将获得一个dict，注意kw获得的dict是extra的一份拷贝，对kw的改动不会影响到函数外的extra')
+print('\n命名关键字参数')
 
 
 
