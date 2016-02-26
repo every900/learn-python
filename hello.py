467f874b28f6a53f29ee5893b249e117c85d1367
houlei <houlei@mail.jj.cn>
2016-02-26 20:14:24 +0800

    20160226

--- hello.py
+++ hello.py
@@ -1005,6 +1005,295 @@ return 11''')
 print(add(-5, 6, abs))
 print('把函数作为参数传入，这样的函数称为高阶函数，函数式编程就是指这种高度抽象的编程范式')
 
+print('''map/reduce
+    如果你读过Google的那篇大名鼎鼎的论文“MapReduce: Simplified Data Processing on Large Clusters”，你就能大概明白map/reduce的概念。
+
+我们先看map。map()函数接收两个参数，一个是函数，一个是Iterable，map将传入的函数依次作用到序列的每个元素，并把结果作为新的Iterator返回。
+
+举例说明，比如我们有一个函数f(x)=x2，要把这个函数作用在一个list [1, 2, 3, 4, 5, 6, 7, 8, 9]上，就可以用map()实现如下：
+
+map''')
+def f(x):
+    return x*x
+print
+r = map(f,[1,2,3,4,5,6,7,8,9])
+print(list(r))
+print('''map()传入的第一个参数是f，即函数对象本身。由于结果r是一个Iterator，Iterator是惰性序列，因此通过list()函数让它把整个序列都计算出来并返回一个list。
+
+你可能会想，不需要map()函数，写一个循环，也可以计算出结果''')
+L = []
+for n in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+    L.append(f(n))
+print(L)
+print('map()作为高阶函数，事实上它把运算规则抽象了，因此，我们不但可以计算简单的f(x)=x2，还可以计算任意复杂的函数，比如，把这个list所有数字转为字符串')
+print(list(map(str, [1, 2, 3, 4, 5, 6, 7, 8, 9])))
+#print 'hello world'
+print('reduce把一个函数作用在一个序列[x1, x2, x3, ...]上，这个函数必须接收两个参数，reduce把结果继续和序列的下一个元素做累积计算')
+#reduce(f, [x1, x2, x3, x4]) = f(f(f(x1, x2), x3), x4)
+from functools import reduce
+def add(x, y):
+    return x + y
+print(reduce(add, [1, 3, 5, 7, 9]))
+from functools import reduce
+def fn(x, y):
+    return x * 10 + y
+print(reduce(fn,[1,3,5,7,9]))
+def char2num(s):
+    return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
+print(reduce(fn, map(char2num, '13579')))
+from functools import reduce
+def str2int(s):
+    def fn(x, y):
+        return x * 10 + y
+    def char2num(s):
+        return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
+    return reduce(fn, map(char2num, s))
+
+from functools import reduce
+def char2num(s):
+    return {'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}[s]
+
+def  str2int(s):
+    return reduce(lambda x,y:x*10+y,map(char2num,s))
+
+print(str2int('13579'))
+
+print('利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字')
+def normalize(name):
+    return name[:1].upper()+name[1:].lower()
+L1 = ['adam', 'LISA', 'barT']
+L2 = list(map(normalize, L1))
+print(L2)
+
+print('Python提供的sum()函数可以接受一个list并求和，请编写一个prod()函数，可以接受一个list并利用reduce()求积')
+def prod(L):
+    return reduce((lambda x,y:x*y),L)
+print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
+
+print('利用map和reduce编写一个str2float函数，把字符串\'123.456\'转换成浮点数123.456')
+#没搞懂
+def str2float(s):
+    l = s.split('.')
+    def fn(x, y):
+        return x * 10 + y
+    def char2num(s):
+        return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
+    return reduce(fn, map(char2num, l[0]))+reduce(fn, map(char2num, l[1]))/pow(10,len(l[1]))
+
+print('str2float(\'123.456\') =', str2float('123.456'))
+
+print('\nfilter过滤器')
+print('''Python内建的filter()函数用于过滤序列。
+
+和map()类似，filter()也接收一个函数和一个序列。和map()不同的是，filter()把传入的函数依次作用于每个元素，然后根据返回值是True还是False决定保留还是丢弃该元素。''')
+def is_odd(n):
+    return n % 2 == 1
+print(list(filter(is_odd,[1,2,4,5,6,9,10,15])))
+
+def not_empty(s):
+    return s and s.strip()
+print(list(filter(not_empty,['A','','B',None,'C','d',''])))
+print('''可见用filter()这个高阶函数，关键在于正确实现一个“筛选”函数。
+
+注意到filter()函数返回的是一个Iterator，也就是一个惰性序列，所以要强迫filter()完成计算结果，需要用list()函数获得所有结果并返回list。
+
+用filter求素数
+
+计算素数的一个方法是埃氏筛法，它的算法理解起来非常简单：
+
+首先，列出从2开始的所有自然数，构造一个序列：
+
+2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ...
+
+取序列的第一个数2，它一定是素数，然后用2把序列的2的倍数筛掉：
+
+3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ...
+
+取新序列的第一个数3，它一定是素数，然后用3把序列的3的倍数筛掉：
+
+5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ...
+
+取新序列的第一个数5，然后用5把序列的5的倍数筛掉：
+
+7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ...
+
+不断筛下去，就可以得到所有的素数。''')
+
+def _odd_iter():
+    n = 1
+    while True:
+        n = n+2
+        yield n
+def _not_divisible(n):
+    return lambda x:x % n > 0
+def primes():
+    yield 2
+    it = _odd_iter()#初始序列
+    while True:
+        n = next(it)#返回序列的第一个数
+        yield n
+        it = filter(_not_divisible(n),it)#构造新序列
+
+#这个生成器先返回第一个素数2，然后，利用filter()不断产生筛选后的新的序列。
+#由于primes()也是一个无限序列，所以调用时需要设置一个退出循环的条件：
+#打印1000以内的素数
+for n in primes():
+    if  n < 10:
+        print(n)
+    else:
+        break
+#注意到Iterator是惰性计算的序列，所以我们可以用Python表示“全体自然数”，“全体素数”这样的序列，而代码非常简洁。
+
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+#测试：
+output = filter(is_palindrome, range(1, 1000))
+print(list(output))
+print('\nfilter()的作用是从一个序列中筛出符合条件的元素。由于filter()使用了惰性计算，所以只有在取filter()结果的时候，才会真正筛选并每次返回下一个筛出的元素。')
+print('''\nsorted
+    排序算法
+
+排序也是在程序中经常用到的算法。无论使用冒泡排序还是快速排序，排序的核心是比较两个元素的大小。如果是数字，我们可以直接比较，但如果是字符串或者两个dict呢？直接比较数学上的大小是没有意义的，因此，比较的过程必须通过函数抽象出来。''')
+print(sorted([36, 5, -12, 9, -21]))
+#sorted()函数也是一个高阶函数，它还可以接收一个key函数来实现自定义的排序，例如按绝对值大小排序
+print(sorted([36,5,-12,9,-21],key=abs))
+print('''key指定的函数将作用于list的每一个元素上，并根据key函数返回的结果进行排序。对比原始的list和经过key=abs处理过的list：
+
+list = [36, 5, -12, 9, -21]
+
+keys = [36, 5,  12, 9,  21]
+然后sorted()函数按照keys进行排序，并按照对应关系返回list相应的元素：
+
+keys排序结果 => [5, 9,  12,  21, 36]
+                |  |    |    |   |
+最终结果     => [5, 9, -12, -21, 36]''')
+print(sorted(['bob', 'about', 'Zoo', 'Credit']))
+print('''默认情况下，对字符串排序，是按照ASCII的大小比较的，由于'Z' < 'a'，结果，大写字母Z会排在小写字母a的前面。
+
+现在，我们提出排序应该忽略大小写，按照字母序排序。要实现这个算法，不必对现有代码大加改动，只要我们能用一个key函数把字符串映射为忽略大小写排序即可。忽略大小写来比较两个字符串，实际上就是先把字符串都变成大写（或者都变成小写），再比较。
+
+这样，我们给sorted传入key函数，即可实现忽略大小写的排序：''')
+print(sorted(['bob', 'about', 'Zoo', 'Credit'],key=str.lower))
+print(sorted(['bob', 'about', 'Zoo', 'Credit'],key=str.upper))
+#要进行反向排序，不必改动key函数，可以传入第三个参数reverse=True：
+print(sorted(['bob', 'about', 'Zoo', 'Credit'],key=str.upper,reverse=True))
+#高阶函数的抽象能力是非常强大的，而且，核心代码可以保持得非常简洁。
+#sorted()也是一个高阶函数。用sorted()排序的关键在于实现一个映射函数。
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+def by_name(t):
+    return t[0]
+
+L2 = sorted(L, key=by_name)
+print(L2)
+
+def by_score(t):
+    return t[1]
+L2 = sorted(L,key=by_score,reverse=True)
+print(L2)
+print('''\n返回函数
+    函数作为返回值
+
+高阶函数除了可以接受函数作为参数外，还可以把函数作为结果值返回。''')
+def calc_sum(*args):
+    ax = 0
+    for n in args:
+        ax = ax + n
+    return ax
+def lazy_sum(*args):
+    def sum():
+        ax = 0
+        for n in args:
+            ax = ax + n
+        return ax
+    return sum
+f = lazy_sum(1, 3, 5, 7, 9)
+print(f)
+print(f())
+print('''在这个例子中，我们在函数lazy_sum中又定义了函数sum，并且，内部函数sum可以引用外部函数lazy_sum的参数和局部变量，当lazy_sum返回函数sum时，相关参数和变量都保存在返回的函数中，这种称为“闭包（Closure）”的程序结构拥有极大的威力
+    请再注意一点，当我们调用lazy_sum()时，每次调用都会返回一个新的函数，即使传入相同的参数：''')
+f1 = lazy_sum(1, 3, 5, 7, 9)
+f2 = lazy_sum(1, 3, 5, 7, 9)
+print(f1==f2)
+print('f1()和f2()的调用结果互不影响。')
+print(f1())
+print(f2())
+print('''\n闭包
+
+注意到返回的函数在其定义内部引用了局部变量args，所以，当一个函数返回了一个函数后，其内部的局部变量还被新函数引用，所以，闭包用起来简单，实现起来可不容易。
+
+另一个需要注意的问题是，返回的函数并没有立刻执行，而是直到调用了f()才执行。''')
+
+def count():
+    fs = []
+    for i in range(1, 4):
+        def f():
+             return i*i
+        fs.append(f)
+    return fs
+
+f1, f2, f3 = count()
+
+#在上面的例子中，每次循环，都创建了一个新的函数，然后，把创建的3个函数都返回了。
+
+#你可能认为调用f1()，f2()和f3()结果应该是1，4，9，但实际结果是：
+print(f1())
+print(f2())
+print(f3())
+print('''全部都是9！原因就在于返回的函数引用了变量i，但它并非立刻执行。等到3个函数都返回时，它们所引用的变量i已经变成了3，因此最终结果为9。
+
+返回闭包时牢记的一点就是：返回函数不要引用任何循环变量，或者后续会发生变化的变量。
+
+如果一定要引用循环变量怎么办？方法是再创建一个函数，用该函数的参数绑定循环变量当前的值，无论该循环变量后续如何更改，已绑定到函数参数的值不变''')
+
+def count():
+    def f(j):
+        def g():
+            return j*j
+        return g
+    fs = []
+    for i in range(1, 4):
+        fs.append(f(i)) # f(i)立刻被执行，因此i的当前值被传入f()
+    return fs
+
+f1, f2, f3 = count()
+print(f1())
+print(f2())
+print(f3())
+#缺点是代码较长，可利用lambda函数缩短代码。
+print('''小结
+
+一个函数可以返回一个计算结果，也可以返回一个函数。
+
+返回一个函数时，牢记该函数并未执行，返回函数中不要引用任何可能会变化的变量。''')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
