a2bf3826ac34080163cd75a1cc2d120c5ca2d4f3
houlei <houlei@mail.jj.cn>
2016-06-06 21:30:53 +0800

    20160606

---
+++ database.py
@@ -0,0 +1,288 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+print('访问数据库')
+print('''
+
+程序运行的时候，数据都是在内存中的。当程序终止的时候，通常都需要将数据保存到磁盘上，无论是保存到本地磁盘，还是通过网络保存到服务器上，最终都会将数据写入磁盘文件。
+
+而如何定义数据的存储格式就是一个大问题。如果我们自己来定义存储格式，比如保存一个班级所有学生的成绩单：
+
+名字	成绩
+Michael	99
+Bob	85
+Bart	59
+Lisa	87
+你可以用一个文本文件保存，一行保存一个学生，用,隔开：
+
+Michael,99
+Bob,85
+Bart,59
+Lisa,87
+你还可以用JSON格式保存，也是文本文件：
+
+[
+    {"name":"Michael","score":99},
+    {"name":"Bob","score":85},
+    {"name":"Bart","score":59},
+    {"name":"Lisa","score":87}
+]
+你还可以定义各种保存格式，但是问题来了：
+
+存储和读取需要自己实现，JSON还是标准，自己定义的格式就各式各样了；
+不能做快速查询，只有把数据全部读到内存中才能自己遍历，但有时候数据的大小远远超过了内存（比如蓝光电影，40GB的数据），根本无法全部读入内存。
+
+为了便于程序保存和读取数据，而且，能直接通过条件快速查询到指定的数据，就出现了数据库（Database）这种专门用于集中存储和查询的软件。
+
+数据库软件诞生的历史非常久远，早在1950年数据库就诞生了。经历了网状数据库，层次数据库，我们现在广泛使用的关系数据库是20世纪70年代基于关系模型的基础上诞生的。
+
+关系模型有一套复杂的数学理论，但是从概念上是十分容易理解的。举个学校的例子：
+
+假设某个XX省YY市ZZ县第一实验小学有3个年级，要表示出这3个年级，可以在Excel中用一个表格画出来：
+
+grade
+
+每个年级又有若干个班级，要把所有班级表示出来，可以在Excel中再画一个表格：
+
+class
+
+这两个表格有个映射关系，就是根据Grade_ID可以在班级表中查找到对应的所有班级：
+
+grade-classes
+
+也就是Grade表的每一行对应Class表的多行，在关系数据库中，这种基于表（Table）的一对多的关系就是关系数据库的基础。
+
+根据某个年级的ID就可以查找所有班级的行，这种查询语句在关系数据库中称为SQL语句，可以写成：
+
+SELECT * FROM classes WHERE grade_id = '1';
+结果也是一个表：
+
+---------+----------+----------
+grade_id | class_id | name
+---------+----------+----------
+1        | 11       | 一年级一班
+---------+----------+----------
+1        | 12       | 一年级二班
+---------+----------+----------
+1        | 13       | 一年级三班
+---------+----------+----------
+类似的，Class表的一行记录又可以关联到Student表的多行记录：
+
+class-students
+
+由于本教程不涉及到关系数据库的详细内容，如果你想从零学习关系数据库和基本的SQL语句，推荐Coursera课程：
+
+英文：https://www.coursera.org/course/db
+
+中文：http://c.open.163.com/coursera/courseIntro.htm?cid=12
+
+NoSQL
+
+你也许还听说过NoSQL数据库，很多NoSQL宣传其速度和规模远远超过关系数据库，所以很多同学觉得有了NoSQL是否就不需要SQL了呢？千万不要被他们忽悠了，连SQL都不明白怎么可能搞明白NoSQL呢？
+
+数据库类别
+
+既然我们要使用关系数据库，就必须选择一个关系数据库。目前广泛使用的关系数据库也就这么几种：
+
+付费的商用数据库：
+
+Oracle，典型的高富帅；
+
+SQL Server，微软自家产品，Windows定制专款；
+
+DB2，IBM的产品，听起来挺高端；
+
+Sybase，曾经跟微软是好基友，后来关系破裂，现在家境惨淡。
+
+这些数据库都是不开源而且付费的，最大的好处是花了钱出了问题可以找厂家解决，不过在Web的世界里，常常需要部署成千上万的数据库服务器，当然不能把大把大把的银子扔给厂家，所以，无论是Google、Facebook，还是国内的BAT，无一例外都选择了免费的开源数据库：
+
+MySQL，大家都在用，一般错不了；
+
+PostgreSQL，学术气息有点重，其实挺不错，但知名度没有MySQL高；
+
+sqlite，嵌入式数据库，适合桌面和移动应用。
+
+作为Python开发工程师，选择哪个免费数据库呢？当然是MySQL。因为MySQL普及率最高，出了错，可以很容易找到解决方法。而且，围绕MySQL有一大堆监控和运维的工具，安装和使用很方便。
+
+为了能继续后面的学习，你需要从MySQL官方网站下载并安装MySQL Community Server 5.6，这个版本是免费的，其他高级版本是要收钱的（请放心，收钱的功能我们用不上）。
+
+	''')
+print('使用SQLite')
+print('''
+SQLite是一种嵌入式数据库，它的数据库就是一个文件。由于SQLite本身是C写的，而且体积很小，所以，经常被集成到各种应用程序中，甚至在iOS和Android的App中都可以集成。
+
+Python就内置了SQLite3，所以，在Python中使用SQLite，不需要安装任何东西，直接使用。
+
+在使用SQLite前，我们先要搞清楚几个概念：
+
+表是数据库中存放关系数据的集合，一个数据库里面通常都包含多个表，比如学生的表，班级的表，学校的表，等等。表和表之间通过外键关联。
+
+要操作关系数据库，首先需要连接到数据库，一个数据库连接称为Connection；
+
+连接到数据库后，需要打开游标，称之为Cursor，通过Cursor执行SQL语句，然后，获得执行结果。
+
+Python定义了一套操作数据库的API接口，任何数据库要连接到Python，只需要提供符合Python标准的数据库驱动即可。
+
+由于SQLite的驱动内置在Python标准库中，所以我们可以直接来操作SQLite数据库。
+
+我们在Python交互式命令行实践一下：
+
+# 导入SQLite驱动:
+>>> import sqlite3
+# 连接到SQLite数据库
+# 数据库文件是test.db
+# 如果文件不存在，会自动在当前目录创建:
+>>> conn = sqlite3.connect('test.db')
+# 创建一个Cursor:
+>>> cursor = conn.cursor()
+# 执行一条SQL语句，创建user表:
+>>> cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+<sqlite3.Cursor object at 0x10f8aa260>
+# 继续执行一条SQL语句，插入一条记录:
+>>> cursor.execute('insert into user (id, name) values (\'1\', \'Michael\')')
+<sqlite3.Cursor object at 0x10f8aa260>
+# 通过rowcount获得插入的行数:
+>>> cursor.rowcount
+1
+# 关闭Cursor:
+>>> cursor.close()
+# 提交事务:
+>>> conn.commit()
+# 关闭Connection:
+>>> conn.close()
+我们再试试查询记录：
+
+>>> conn = sqlite3.connect('test.db')
+>>> cursor = conn.cursor()
+# 执行查询语句:
+>>> cursor.execute('select * from user where id=?', ('1',))
+<sqlite3.Cursor object at 0x10f8aa340>
+# 获得查询结果集:
+>>> values = cursor.fetchall()
+>>> values
+[('1', 'Michael')]
+>>> cursor.close()
+>>> conn.close()
+使用Python的DB-API时，只要搞清楚Connection和Cursor对象，打开后一定记得关闭，就可以放心地使用。
+
+使用Cursor对象执行insert，update，delete语句时，执行结果由rowcount返回影响的行数，就可以拿到执行结果。
+
+使用Cursor对象执行select语句时，通过featchall()可以拿到结果集。结果集是一个list，每个元素都是一个tuple，对应一行记录。
+
+如果SQL语句带有参数，那么需要把参数按照位置传递给execute()方法，有几个?占位符就必须对应几个参数，例如：
+
+cursor.execute('select * from user where name=? and pwd=?', ('abc', 'password'))
+SQLite支持常见的标准SQL语句以及几种常见的数据类型。具体文档请参阅SQLite官方网站。
+
+小结
+
+在Python中操作数据库时，要先导入数据库对应的驱动，然后，通过Connection对象和Cursor对象操作数据。
+
+要确保打开的Connection对象和Cursor对象都正确地被关闭，否则，资源就会泄露。
+
+如何才能确保出错的情况下也关闭掉Connection对象和Cursor对象呢？请回忆try:...except:...finally:...的用法。
+
+	''')
+print('使用MySQL')
+print('''
+MySQL是Web世界中使用最广泛的数据库服务器。SQLite的特点是轻量级、可嵌入，但不能承受高并发访问，适合桌面和移动应用。而MySQL是为服务器端设计的数据库，能承受高并发访问，同时占用的内存也远远大于SQLite。
+
+此外，MySQL内部有多种数据库引擎，最常用的引擎是支持数据库事务的InnoDB。
+
+安装MySQL
+
+可以直接从MySQL官方网站下载最新的Community Server 5.6.x版本。MySQL是跨平台的，选择对应的平台下载安装文件，安装即可。
+
+安装时，MySQL会提示输入root用户的口令，请务必记清楚。如果怕记不住，就把口令设置为password。
+
+在Windows上，安装时请选择UTF-8编码，以便正确地处理中文。
+
+在Mac或Linux上，需要编辑MySQL的配置文件，把数据库默认的编码全部改为UTF-8。MySQL的配置文件默认存放在/etc/my.cnf或者/etc/mysql/my.cnf：
+[client]
+default-character-set = utf8
+
+[mysqld]
+default-storage-engine = INNODB
+character-set-server = utf8
+collation-server = utf8_general_ci
+重启MySQL后，可以通过MySQL的客户端命令行检查编码：
+
+$ mysql -u root -p
+Enter password:
+Welcome to the MySQL monitor...
+...
+
+mysql> show variables like '%char%';
++--------------------------+--------------------------------------------------------+
+| Variable_name            | Value                                                  |
++--------------------------+--------------------------------------------------------+
+| character_set_client     | utf8                                                   |
+| character_set_connection | utf8                                                   |
+| character_set_database   | utf8                                                   |
+| character_set_filesystem | binary                                                 |
+| character_set_results    | utf8                                                   |
+| character_set_server     | utf8                                                   |
+| character_set_system     | utf8                                                   |
+| character_sets_dir       | /usr/local/mysql-5.1.65-osx10.6-x86_64/share/charsets/ |
++--------------------------+--------------------------------------------------------+
+8 rows in set (0.00 sec)
+看到utf8字样就表示编码设置正确。
+
+注：如果MySQL的版本≥5.5.3，可以把编码设置为utf8mb4，utf8mb4和utf8完全兼容，但它支持最新的Unicode标准，可以显示emoji字符。
+
+安装MySQL驱动
+
+由于MySQL服务器以独立的进程运行，并通过网络对外服务，所以，需要支持Python的MySQL驱动来连接到MySQL服务器。MySQL官方提供了mysql-connector-python驱动，但是安装的时候需要给pip命令加上参数--allow-external：
+
+$ pip install mysql-connector-python --allow-external mysql-connector-python
+我们演示如何连接到MySQL服务器的test数据库：
+
+# 导入MySQL驱动:
+>>> import mysql.connector
+# 注意把password设为你的root口令:
+>>> conn = mysql.connector.connect(user='root', password='password', database='test')
+>>> cursor = conn.cursor()
+# 创建user表:
+>>> cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+# 插入一行记录，注意MySQL的占位符是%s:
+>>> cursor.execute('insert into user (id, name) values (%s, %s)', ['1', 'Michael'])
+>>> cursor.rowcount
+1
+# 提交事务:
+>>> conn.commit()
+>>> cursor.close()
+# 运行查询:
+>>> cursor = conn.cursor()
+>>> cursor.execute('select * from user where id = %s', ('1',))
+>>> values = cursor.fetchall()
+>>> values
+[('1', 'Michael')]
+# 关闭Cursor和Connection:
+>>> cursor.close()
+True
+>>> conn.close()
+由于Python的DB-API定义都是通用的，所以，操作MySQL的数据库代码和SQLite类似。
+
+小结
+
+执行INSERT等操作后要调用commit()提交事务；
+
+MySQL的SQL占位符是%s。
+
+	''')
+print('使用SQLAlchemy')
+print('''
+数据库表是一个二维表，包含多行多列。把一个表的内容用Python的数据结构表示出来的话，可以用一个list表示多行，list的每一个元素是tuple，表示一行记录，比如，包含id和name的user表：
+
+[
+    ('1', 'Michael'),
+    ('2', 'Bob'),
+    ('3', 'Adam')
+]
+
+
+	''')
+
+
+
+
+
